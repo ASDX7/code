c120981b70847216d9e4d3a96bb6c202ebbf3038
BigWangYuDong <yudongwang1226@gmail.com>
2023-11-26 07:27:20 +0000

    FFA-Net

--- net/test.py
+++ net/test.py
@@ -20,34 +20,36 @@ def tensorShow(tensors,titles=['haze']):
         plt.show()
 
 parser=argparse.ArgumentParser()
-parser.add_argument('--task',type=str,default='its',help='its or ots')
+parser.add_argument('--task',type=str,default='ots',help='its or ots')
 parser.add_argument('--test_imgs',type=str,default='test_imgs',help='Test imgs folder')
 opt=parser.parse_args()
 dataset=opt.task
 gps=3
 blocks=19
-img_dir=abs+opt.test_imgs+'/'
-output_dir=abs+f'pred_FFA_{dataset}/'
-print("pred_dir:",output_dir)
+img_dir='data/Foggy_Driving_COCO/JPEGImages/' 
+output_dir='data/Foggy_Driving_COCO_ffa/JPEGImages/' 
+print("pred_dir:",output_dir)                                                                               # frankfurt lindau munster 
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 model_dir=abs+f'trained_models/{dataset}_train_ffa_{gps}_{blocks}.pk'
-device='cuda' if torch.cuda.is_available() else 'cpu'
+device='cuda:0' if torch.cuda.is_available() else 'cpu'
 ckp=torch.load(model_dir,map_location=device)
 net=FFA(gps=gps,blocks=blocks)
 net=nn.DataParallel(net)
 net.load_state_dict(ckp['model'])
+net.to(device)  # 加了这句话
 net.eval()
 for im in os.listdir(img_dir):
+    # if im[-5] == '2':
     print(f'\r {im}',end='',flush=True)
     haze = Image.open(img_dir+im)
     haze1= tfs.Compose([
         tfs.ToTensor(),
         tfs.Normalize(mean=[0.64, 0.6, 0.58],std=[0.14,0.15, 0.152])
     ])(haze)[None,::]
-    haze_no=tfs.ToTensor()(haze)[None,::]
+    # haze_no=tfs.ToTensor()(haze)[None,::]
     with torch.no_grad():
         pred = net(haze1)
     ts=torch.squeeze(pred.clamp(0,1).cpu())
-    tensorShow([haze_no,pred.clamp(0,1).cpu()],['haze','pred'])
-    vutils.save_image(ts,output_dir+im.split('.')[0]+'_FFA.png')
+    # tensorShow([haze_no,pred.clamp(0,1).cpu()],['haze','pred'])
+    vutils.save_image(ts,output_dir+im.split('.')[0]+'.'+im.split('.')[1]) # _FFA.png
